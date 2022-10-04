c775c8636443df5c8ff1997238a0dd2ed5edc318
Arturo Suárez <manuel.suarez@cimat.mx>
2022-10-04 11:53:18 -0500

    Strategy load

--- main.py
+++ main.py
@@ -8,3 +8,6 @@ print(tf.__version__)
 datasets, info = tfds.load(name='mnist', with_info=True, as_supervised=True)
 
 mnist_train, mnist_test = datasets['train'], datasets['test']
+
+strategy = tf.distribute.MirroredStrategy()
+print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
