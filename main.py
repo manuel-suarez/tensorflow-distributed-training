637c4e8da59a771ddeffc1936931d5562b558358
Arturo Suárez <manuel.suarez@cimat.mx>
2022-10-04 11:55:01 -0500

    Input pipeline

--- main.py
+++ main.py
@@ -11,3 +11,14 @@ mnist_train, mnist_test = datasets['train'], datasets['test']
 
 strategy = tf.distribute.MirroredStrategy()
 print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
+
+# You can also do info.splits.total_num_examples to get the total
+# number of examples in the dataset.
+
+num_train_examples = info.splits['train'].num_examples
+num_test_examples = info.splits['test'].num_examples
+
+BUFFER_SIZE = 10000
+
+BATCH_SIZE_PER_REPLICA = 64
+BATCH_SIZE = BATCH_SIZE_PER_REPLICA * strategy.num_replicas_in_sync
